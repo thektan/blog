f2fa497069c03dd441767dcda6c1e9137cec0a11
Kevin Tan <thektan@gmail.com>
2015-05-15 01:31:24 -0700

    Demo update - animations and responsiveness
    Added shadow to navigation bar once scrolled past post title.

--- demo/js/main.js
+++ demo/js/main.js
@@ -1,5 +1,10 @@
 $(document).ready(function ()
 {
+    // Intro animations.
+    $('.post-container').css('margin-top', '150px')
+                        .css('opacity', '1');
+    $('.fade-in').css('opacity', '1');
+
     var elementPosition = $('.post-container').offset();
 
     // Remove parenthesis tags around images.
@@ -16,10 +21,12 @@ $(document).ready(function ()
         if ($(window).scrollTop() - 50 > elementPosition.top) {
             $('.currently').css('padding-top', '0')
                            .css('opacity', '1');
+            $('#navigation-bar-container').css('box-shadow', '0px 0px 8px 0px rgba(0,0,0,0.12)')
         }
         else {
             $('.currently').css('padding-top', '20px')
                            .css('opacity', '0');;
+           $('#navigation-bar-container').css('box-shadow', '0px 0px 8px 0px rgba(0,0,0,0)')
         }
     });
 
