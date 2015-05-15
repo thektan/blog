60f00556b906196dbd7914b52d5396f61b5a31be
Kevin Tan <thektan@gmail.com>
2015-05-15 12:49:23 -0700

    Initialize Jekyll

--- demo/js/main.js
+++ demo/js/main.js
@@ -21,12 +21,12 @@ $(document).ready(function ()
         if ($(window).scrollTop() - 50 > elementPosition.top) {
             $('.currently').css('padding-top', '0')
                            .css('opacity', '1');
-            $('#navigation-bar-container').css('box-shadow', '0px 0px 8px 0px rgba(0,0,0,0.12)')
+            $('#navigation-bar-container').css('box-shadow', '0px 0px 8px 0px rgba(0,0,0,0.12)');
         }
         else {
             $('.currently').css('padding-top', '20px')
-                           .css('opacity', '0');;
-           $('#navigation-bar-container').css('box-shadow', '0px 0px 8px 0px rgba(0,0,0,0)')
+                           .css('opacity', '0');
+           $('#navigation-bar-container').css('box-shadow', '0px 0px 8px 0px rgba(0,0,0,0)');
         }
     });
 
